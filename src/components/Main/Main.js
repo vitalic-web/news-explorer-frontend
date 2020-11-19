1efc4a1d608c4cd7f73c5a64511eccfa3a9e1fed
Vitalic-web <vitalic.web@gmail.com>
2020-11-19 17:53:36 +0200

    update addMoreResults

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -9,6 +9,7 @@ import Popup from '../Popup/Popup';
 import NewsCardPic1 from '../../images/NewsCard__pic1.png';
 import NewsCardPic2 from '../../images/NewsCard__pic2.png';
 import NewsCardPic3 from '../../images/NewsCard__pic3.png';
+import TestPhoto from '../../images/photo-1605462398512-51e939d2d21d.jpg';
 
 // импорт компонентов "Нет результатов" и "Прелоудер" для проверки
 // добавлены в разметку и закомментированы в конце страницы после компонента "Об авторе"
@@ -16,7 +17,8 @@ import NoResult from '../NoResult/NoResult';
 import Preloader from '../Preloader/Preloader';
 
 function Main() {
-  const [searchResultMain, setSearchResultMain] = useState([
+  // результаты поиска новостей
+  const searchResult = [
     {
       _id: 1,
       link: NewsCardPic1,
@@ -44,11 +46,7 @@ function Main() {
       sourceName: "РИА",
       sourceLink: "https://ria.ru/"
     },
-  ]);
-
-  // метод добавления результатов при нажатии на кнопку "показать еще"
-  const addMoreResults = () => {
-    setSearchResultMain([...searchResultMain, {
+    {
       _id: 4,
       link: NewsCardPic1,
       date: "2 августа, 2019",
@@ -67,8 +65,63 @@ function Main() {
       sourceName: "МЕДУЗА",
       sourceLink: "https://meduza.io/",
       tag: "Фотография"
-    },]);
-  };
+    },
+    {
+      _id: 6,
+      link: NewsCardPic1,
+      date: "2 августа, 2019",
+      title: "3Национальное достояние – парки",
+      text: "В 2016 году Америка отмечала важный юбилей: сто лет назад здесь начала складываться система национальных парков – охраняемых территорий, где и сегодня каждый может приобщиться к природе.",
+      sourceName: "ЛЕНТА.РУ",
+      sourceLink: "https://lenta.ru/"
+    },
+    {
+      _id: 7,
+      link: NewsCardPic2,
+      date: "2 августа, 2019",
+      title: "4Лесные огоньки: история одной фотографии",
+      text: "Фотограф отвлеклась от освещения суровой политической реальности Мексики, чтобы запечатлеть ускользающую красоту одного из местных чудес природы.",
+      sourceName: "МЕДУЗА",
+      sourceLink: "https://meduza.io/"
+    },
+    {
+      _id: 8,
+      link: NewsCardPic3,
+      date: "2 августа, 2019",
+      title: "5Первозданная тайга»: новый фотопроект Игоря Шпиленка",
+      text: "Знаменитый фотограф снимает первозданные леса России, чтобы рассказать о необходимости их сохранения. В этот раз он отправился в Двинско-Пинежскую тайгу, где dfgdgdfgdfg dfgdf gdfgdfd gdgdfgdf dgdgdf dfgdfg  dfgdf g",
+      sourceName: "РИА",
+      sourceLink: "https://ria.ru/"
+    },
+    {
+      _id: 9,
+      link: NewsCardPic1,
+      date: "2 августа, 2019",
+      title: "6Национальное достояние – парки",
+      text: "В 2016 году Америка отмечала важный юбилей: сто лет назад здесь начала складываться система национальных парков – охраняемых территорий, где и сегодня каждый может приобщиться к природе.",
+      sourceName: "ЛЕНТА.РУ",
+      sourceLink: "https://lenta.ru/",
+      tag: "Парки"
+    },
+    {
+      _id: 10,
+      link: NewsCardPic2,
+      date: "2 августа, 2019",
+      title: "7Лесные огоньки: история одной фотографии",
+      text: "Фотограф отвлеклась от освещения суровой политической реальности Мексики, чтобы запечатлеть ускользающую красоту одного из местных чудес природы.",
+      sourceName: "МЕДУЗА",
+      sourceLink: "https://meduza.io/",
+      tag: "Фотография"
+    },
+  ]
+
+  // стейт для выгрузки нужного количества новостей на страницу
+  const [searchResultMain, setSearchResultMain] = useState(searchResult.slice(0, 3));
+
+  // метод для добавляения новых карточек при нажатии на кнопку Показать еще
+  const addMoreResults = () => {
+    setSearchResultMain([...searchResultMain, ...searchResult.slice(searchResultMain.length, searchResultMain.length + 3)]);
+  }
 
   const [isOpen, setIsOpen] = useState(false);
 
@@ -97,7 +150,6 @@ function Main() {
     }
   })
 
-  // сделать вывод по три новости
   // сделать вывод текущей даты в нужном формате
 
   return (
--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -2,35 +2,44 @@ import './Popup.css';
 import { useState, useEffect } from 'react';
 
 function Popup(props) {
-  const [validationMessage, setValidationMessage] = useState({ email: '', password: '', name: '' });
-  const [validityEmail, setValidityEmail] = useState(false);
-  const [validityPassword, setValidityPassword] = useState(false);
-  const [validityName, setValidityName] = useState(false);
-  const [buttonDisabled, setButtonDisabled] = useState(true);
-  const [registration, setRegistration] = useState(false);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [name, setName] = useState('');
+  const [validationMessage, setValidationMessage] = useState({ email: '', password: '', name: '' }); // кастомные тексты ошибок
+  const [validityEmail, setValidityEmail] = useState(false); // валидность емейл
+  const [validityPassword, setValidityPassword] = useState(false); // валидность пароля
+  const [validityName, setValidityName] = useState(false); // валидность имени
+  const [buttonDisabled, setButtonDisabled] = useState(true); // блокировка кнопки
+  const [registration, setRegistration] = useState(false); // показ/скрытие попапа регистрации
+  const [email, setEmail] = useState(''); // значение инпута емейл
+  const [password, setPassword] = useState(''); // значение инпута пароль
+  const [name, setName] = useState(''); // значение инпута имя
+
+  // стейт проверки юзера в базе, оставил в состоянии true чтобы было видно верстку
   const [sameUser, setSameUser] = useState(true);
 
+  // показ/скрытие попапа с формой, при отрицательном значении показ/скрытие попапа успешной регистрации
+  const [popupWithForm, setPopupWithForm] = useState(true);
+
+  // валидация поля емейл
   const validateEmail = (e) => {
     setEmail(e.target.value);
     setValidationMessage({ email: 'Неправильный формат email' });
     setValidityEmail(e.target.validity.valid);
   }
 
+  // валидация поля пароль
   const validatePassword = (e) => {
     setPassword(e.target.value);
     setValidationMessage({ password: 'Пароль должен быть не менее 6 символов' });
     setValidityPassword(e.target.validity.valid);
   }
 
+  // валидация поля имя
   const validateName = (e) => {
     setName(e.target.value);
     setValidationMessage({ name: 'Имя должно быть не менее 2 символов' });
     setValidityName(e.target.validity.valid);
   }
 
+  // переключение попапов вход/регистрация, так же очистка инпутов от введенных данных
   const toggleAuth = () => {
     setRegistration(!registration);
     setButtonDisabled(true);
@@ -39,6 +48,22 @@ function Popup(props) {
     setName('');
   }
 
+  // показ попапа успешной регистрации и скрытие попапа с формой
+  const showSuccessPopup = (e) => {
+    e.preventDefault();
+
+    setPopupWithForm(false);
+  }
+
+  // скрытие попапа успешной регистрации и показ попапа вход
+  const redirectLogin = () => {
+    setRegistration(!registration);
+    setPopupWithForm(true);
+    setEmail('');
+    setPassword('');
+  }
+
+  // блокировка кнопок обеих форм
   useEffect(() => {
     if (registration) {
       if (validityEmail && validityPassword && validityName) {
@@ -55,7 +80,8 @@ function Popup(props) {
   return (
     <section className={`Popup ${props.isOpen && 'Popup_active'}`}>
 
-      <form className="Popup__container" onSubmit={props.onSubmit}>
+      {/* по умолчанию открывается попап входа */}
+      <form className={`Popup__container ${!popupWithForm && 'Popup__container_hidden'}`} onSubmit={props.onSubmit}>
         <h3 className="Popup__title">{registration ? 'Регистрация' : 'Вход'}</h3>
 
         <label className="Popup__input-label" htmlFor="email">Email</label>
@@ -72,22 +98,34 @@ function Popup(props) {
           <span className="Popup__input-error-message" id="password-input-error">{!validityPassword && validationMessage.password}</span>
         </div>
 
+        {/* попап регистрации, зависит от состояния переменной registration */}
         {registration &&
           <>
             <label className="Popup__input-label" htmlFor="password">Имя</label>
             <div className="Popup__input-area">
               <input className="Popup__input" onChange={validateName} name="name" type="text" required
-                value={name} minLength="2" maxLength="40" id="name" placeholder="Введите своё имя" />
+                value={name} minLength="2" maxLength="40" id="name" placeholder="Введите своё имя"  />
               <span className="Popup__input-error-message" id="name-input-error">{!validityName && validationMessage.name}</span>
               {sameUser && <span className="Popup__input-error-message Popup__input-error-message_regisrate">Такой пользователь уже есть</span>}
             </div>
           </>
         }
 
-        <button className={`Popup__save-button ${buttonDisabled && 'Popup__save-button_disabled'}`} type="submit" disabled={buttonDisabled}>{registration ? 'Зарегистрироваться' : 'Войти'}</button>
-      <p className="Popup__auth">Или <span className="Popup__auth-link" onClick={toggleAuth}>{registration ? 'Войти' : 'Зарегистрироваться'}</span></p>
+        <button className={`Popup__save-button ${buttonDisabled && 'Popup__save-button_disabled'}`}
+          onClick={registration ? showSuccessPopup : undefined} type="submit" disabled={buttonDisabled}>{registration ? 'Зарегистрироваться' : 'Войти'}</button>
+        <p className="Popup__auth">Или <span className="Popup__auth-link" onClick={toggleAuth}>{registration ? 'Войти' : 'Зарегистрироваться'}</span></p>
         <button className="Popup__close-icon" type="button" onClick={props.onClose} />
       </form>
+
+      {/* попап успешной регистрации, зависит от отрицательного состояния переменной popupWithForm */}
+      {!popupWithForm &&
+        <form className="Popup__container" onSubmit={props.onSubmit}>
+          <h3 className="Popup__title Popup__title_successful">Пользователь успешно зарегистрирован!</h3>
+          <button className="Popup__close-icon" type="button" onClick={props.onClose} />
+          <p className="Popup__success-login" onClick={redirectLogin}>Войти</p>
+        </form>
+      }
+
     </section>
   );
 }
